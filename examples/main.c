5ef097e1b9b6e179ef4c40aaa2af6d5827fc11bc
zanadoman <domanseven@gmail.com>
2025-07-21 20:14:12 +0200

    example main cleanup

--- examples/main.c
+++ examples/main.c
@@ -1,3 +1,28 @@
+/* clang-format off */
+
+/*
+  FlyGPU
+  Copyright (C) 2025 Domán Zana
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "../include/flygpu/flygpu.h"
+
 #include <SDL3/SDL_error.h>
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_init.h>
@@ -6,49 +31,65 @@
 #include <SDL3/SDL_timer.h>
 #include <SDL3/SDL_video.h>
 
-#include "../include/flygpu/flygpu.h"
-
 Sint32 main(void)
 {
+    SDL_Window  *window   = NULL;
+    FG_Renderer *renderer = NULL;
+    Uint64       tick     = 0;
+    bool         running  = true;
+    SDL_Event    event;
+    FG_Quad3     quads[2] = {
+        [0] = {
+            .transform = {
+                .translation.z = -1.0F,
+                .scale = {
+                    .x = 0.1F,
+                    .y = 0.1F
+                }
+            }
+        },
+        [1] = {
+            .transform = {
+                .translation = {
+                    .x = 0.5F,
+                    .y = 0.5F,
+                    .z = -5.0F
+                },
+                .rotation = -45.0F / 180.0F * 3.14F,
+                .scale = {
+                    .x = 1.2F,
+                    .y = 1.0F
+                }
+            }
+        }
+    };
+
     if (!SDL_InitSubSystem(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window *window =
-        SDL_CreateWindow("SDL_GPU", 800, 600, SDL_WINDOW_RESIZABLE);
+    window = SDL_CreateWindow("SDL_GPU", 800, 600, SDL_WINDOW_RESIZABLE);
     if (!window) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s\n", SDL_GetError());
         return 1;
     }
 
-    FG_Renderer *renderer = FG_CreateRenderer(window, false);
+    renderer = FG_CreateRenderer(window, false);
     if (!renderer) {
         SDL_LogError(SDL_LOG_CATEGORY_GPU, "%s\n", SDL_GetError());
         return 1;
     }
 
-    FG_Quad3 quads[2] = {
-        [0] = {.transform = {.t.z = -1.0F, .s.x = 0.1F, .s.y = 0.1F}},
-        [1] = {.transform = {.t.x = 0.5F,
-                             .t.y = 0.5F,
-                             .t.z = -5.0F,
-                             .rotation   = -45.0F / 180.0F * 3.14F,
-                             .s.x = 1.2F,
-                             .s.y = 1.0F}}};
-
-    bool   running = true;
-    Uint64 tick    = SDL_GetTicks();
+    tick = SDL_GetTicks();
 
     while (running) {
-        SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if ((event.type) == SDL_EVENT_QUIT) running = false;
         }
 
-        if (!FG_RendererDraw(renderer, quads,
-                             quads + sizeof(quads) / sizeof(*quads))) {
-            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s\n", SDL_GetError());
+        if (!FG_RendererDraw(renderer, quads, quads + sizeof(quads) / sizeof(*quads))) {
+            SDL_LogError(SDL_LOG_CATEGORY_GPU, "%s\n", SDL_GetError());
             return 1;
         }
 
