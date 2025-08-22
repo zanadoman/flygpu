d976aee2bb9897f37f587557c8bf85e255eb7ee5
zanadoman <domanseven@gmail.com>
2025-08-22 22:19:17 +0200

    refined shader input and output

--- shaders/quad3.frag.glsl
+++ shaders/quad3.frag.glsl
@@ -1,17 +1,20 @@
-#version 460
+#version 460 core
 
 layout(set = 2, binding = 0) uniform sampler2D albedoSampler;
 layout(set = 2, binding = 1) uniform sampler2D alphaSampler;
 layout(set = 2, binding = 2) uniform sampler2D specularSampler;
 layout(set = 2, binding = 3) uniform sampler2D normalSampler;
 
-layout(location = 0) in mat3 TBN;
-layout(location = 3) in vec3 fragPosition;
-layout(location = 4) in vec3 fragColorTL;
-layout(location = 5) in vec3 fragColorBL;
-layout(location = 6) in vec3 fragColorBR;
-layout(location = 7) in vec3 fragColorTR;
-layout(location = 8) in vec2 fragTexCoord;
+layout(location = 0) in struct
+{
+    mat3 TBN;
+    vec3 position;
+    vec3 colorTL;
+    vec3 colorBL;
+    vec3 colorBR;
+    vec3 colorTR;
+    vec2 texCoord;
+} frag;
 
 layout(location = 0) out vec3 outPosition;
 layout(location = 1) out vec3 outNormal;
@@ -20,17 +23,17 @@ layout(location = 3) out vec3 outAlbedo;
 
 void main()
 {
-    if (texture(alphaSampler, fragTexCoord).a <= 0.0F) discard;
+    if (texture(alphaSampler, frag.texCoord).a <= 0.0F) discard;
 
-    const vec2 fragColorCoord = fract(fragTexCoord);
+    const vec2 fragColorCoord = fract(frag.texCoord);
     const vec3 fragColor      = mix(
-        mix(fragColorTL, fragColorTR, fragColorCoord.x),
-        mix(fragColorBL, fragColorBR, fragColorCoord.x),
+        mix(frag.colorTL, frag.colorTR, fragColorCoord.x),
+        mix(frag.colorBL, frag.colorBR, fragColorCoord.x),
         fragColorCoord.y
     );
 
-    outPosition = fragPosition;
-    outNormal   = TBN * (texture(normalSampler, fragTexCoord).rgb * 2.0F - 1.0F);
-    outSpecular = fragColor * texture(specularSampler, fragTexCoord).rgb;
-    outAlbedo   = fragColor * texture(albedoSampler, fragTexCoord).rgb;
+    outPosition = frag.position;
+    outNormal   = frag.TBN * (texture(normalSampler, frag.texCoord).rgb * 2.0F - 1.0F);
+    outSpecular = fragColor * texture(specularSampler, frag.texCoord).rgb;
+    outAlbedo   = fragColor * texture(albedoSampler, frag.texCoord).rgb;
 }
--- shaders/quad3.vert.glsl
+++ shaders/quad3.vert.glsl
@@ -1,29 +1,24 @@
-#version 460
+#version 460 core
 
-layout(location = 0)  in vec4 MODEL0;
-layout(location = 1)  in vec4 MODEL1;
-layout(location = 2)  in vec4 MODEL2;
-layout(location = 3)  in vec4 MODEL3;
-layout(location = 4)  in vec4 MVP0;
-layout(location = 5)  in vec4 MVP1;
-layout(location = 6)  in vec4 MVP2;
-layout(location = 7)  in vec4 MVP3;
-layout(location = 8)  in vec3 TBN0;
-layout(location = 9)  in vec3 TBN1;
-layout(location = 10) in vec3 TBN2;
+layout(location = 0)  in mat4 MODEL;
+layout(location = 4)  in mat4 MVP;
+layout(location = 8)  in mat3 TBN;
 layout(location = 11) in vec3 inColorTL;
 layout(location = 12) in vec3 inColorBL;
 layout(location = 13) in vec3 inColorBR;
 layout(location = 14) in vec3 inColorTR;
 layout(location = 15) in vec4 inTexCoord;
 
-layout(location = 0) out mat3 TBN;
-layout(location = 3) out vec3 fragPosition;
-layout(location = 4) out vec3 fragColorTL;
-layout(location = 5) out vec3 fragColorBL;
-layout(location = 6) out vec3 fragColorBR;
-layout(location = 7) out vec3 fragColorTR;
-layout(location = 8) out vec2 fragTexCoord;
+layout(location = 0) out struct
+{
+    mat3 TBN;
+    vec3 position;
+    vec3 colorTL;
+    vec3 colorBL;
+    vec3 colorBR;
+    vec3 colorTR;
+    vec2 texCoord;
+} frag;
 
 const uint indices[6] = uint[](0, 1, 3, 1, 2, 3);
 
@@ -38,14 +33,14 @@ void main()
 {
     const uint i = indices[gl_VertexIndex];
 
-    gl_Position  = mat4(MVP0, MVP1, MVP2, MVP3) * positions[i];
-    TBN          = mat3(TBN0, TBN1, TBN2);
-    fragPosition = (mat4(MODEL0, MODEL1, MODEL2, MODEL3) * positions[i]).xyz;
-    fragColorTL  = inColorTL;
-    fragColorBL  = inColorBL;
-    fragColorBR  = inColorBR;
-    fragColorTR  = inColorTR;
-    fragTexCoord = vec2[](
+    gl_Position   = MVP * positions[i];
+    frag.TBN      = TBN;
+    frag.position = (MODEL * positions[i]).xyz;
+    frag.colorTL  = inColorTL;
+    frag.colorBL  = inColorBL;
+    frag.colorBR  = inColorBR;
+    frag.colorTR  = inColorTR;
+    frag.texCoord = vec2[](
         inTexCoord.xy,
         vec2(inTexCoord.x, inTexCoord.w),
         inTexCoord.zw,
