0f8f581958a036b9e2c75c0feaaadf959672723c
zanadoman <domanseven@gmail.com>
2025-10-03 16:04:34 +0200

    fixed infinite direct light calculation

--- shaders/shading.frag.hlsl
+++ shaders/shading.frag.hlsl
@@ -57,72 +57,52 @@ ByteAddressBuffer             bDirects  : register(t4, space2);
 ByteAddressBuffer             bOmnis    : register(t5, space2);
 ConstantBuffer<UniformBuffer> cUniform  : register(b0, space3);
 
-static float3 Normal;
-static float3 Specular;
-static float3 Albedo;
-static float3 Output;
-static float3 ViewDir;
-static float  Attenuation;
-static float3 LightDir;
-static float  NdotL;
-
-static void accumulate(const float3 color);
-
-void accumulate(const float3 color)
-{
-    Output += (Albedo
-            * NdotL
-            * color
-            + Specular
-            * pow(
-                  max(dot(Normal, normalize(LightDir + ViewDir)), 0.0F),
-                  cUniform.Shine
-              )
-            * color)
-            * Attenuation;
-}
-
 float4 main(const noperspective float2 TexCoord : TEXCOORD0) : SV_Target0
 {
-    Normal = tNormal.Sample(sNormal, TexCoord).xyz;
-    if (all(Normal == 0.0F)) discard;
+    float3 normal = tNormal.Sample(sNormal, TexCoord).xyz;
+    if (all(normal == 0.0F)) discard;
 
-                 Normal   = normalize(Normal);
-                 Specular = tSpecular.Sample(sSpecular, TexCoord).rgb;
-                 Albedo   = tAlbedo.Sample(sAlbedo, TexCoord).rgb;
-                 Output   = Albedo * cUniform.Ambient;
-    const float3 position = tPosition.Sample(sPosition, TexCoord).xyz;
-                 ViewDir  = normalize(cUniform.Origo - position);
-
-    Attenuation = 1.0F;
+                 normal   = normalize(normal);
+    const float3 albedo   = tAlbedo.Sample(sAlbedo, TexCoord).rgb;
+    float3       output   = albedo * cUniform.Ambient;
+    const float3 specular = tSpecular.Sample(sSpecular, TexCoord).rgb;
 
     for (uint i = 0; i != cUniform.DirectsSize; i += sizeof(DirectLight)) {
         const DirectLight light = bDirects.Load<DirectLight>(i);
+        const float       NdotL = dot(normal, normalize(-light.Direction));
 
-        LightDir = normalize(-light.Direction);
-
-        NdotL = dot(Normal, LightDir);
-        if (0.0F <= NdotL) accumulate(light.Color);
+        if (0.0F <= NdotL) {
+            output += albedo * NdotL * light.Color
+                    + specular * pow(NdotL, cUniform.Shine) * light.Color;
+        }
     }
+
+    const float3 position = tPosition.Sample(sPosition, TexCoord).xyz;
+    const float3 viewDir  = normalize(cUniform.Origo - position);
 
     for (uint i = 0; i != cUniform.OmnisSize; i += sizeof(OmniLight)) {
-        const OmniLight light = bOmnis.Load<OmniLight>(i);
+        const OmniLight light    = bOmnis.Load<OmniLight>(i);
+        float3          lightDir = light.Position - position;
+        const float     distance = length(lightDir);
 
-        LightDir = light.Position - position;
-
-        const float distance = length(LightDir);
         if (light.Radius <= distance || distance == 0.0F) continue;
 
-        LightDir /= distance;
+                    lightDir /= distance;
+        const float NdotL     = dot(normal, lightDir);
 
-        NdotL = dot(Normal, LightDir);
-        if (NdotL < 0.0F) continue;
+        if (0.0F <= NdotL) {
+            const float attenuation = distance / light.Radius;
 
-        Attenuation = distance / light.Radius;
-        Attenuation = 1.0F - Attenuation * Attenuation;
-
-        accumulate(light.Color);
+            output += (albedo * NdotL * light.Color
+                    + specular
+                    * pow(
+                          max(dot(normal, normalize(lightDir + viewDir)), 0.0F),
+                          cUniform.Shine
+                      )
+                    * light.Color)
+                    * (1.0F - attenuation * attenuation);
+        }
     }
 
-    return float4(Output, 1.0F);
+    return float4(output, 1.0F);
 }
